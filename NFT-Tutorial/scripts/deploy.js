08814c153757039002bf3679837c01bb162a0f65
Salah <2236291956@qq.com>
2022-09-21 15:51:45 +0800

    'firstcommit'

---
+++ NFT-Tutorial/scripts/deploy.js
@@ -0,0 +1,35 @@
+/*
+ * @Author: Salah 2236291956@qq.com
+ * @Date: 2022-09-21 11:35:25
+ * @LastEditors: Salah 2236291956@qq.com
+ * @LastEditTime: 2022-09-21 13:58:42
+ * @FilePath: \NFT-Tutorial\scripts\deploy.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+// Import ethers from Hardhat package
+const { ethers } = require("hardhat");
+
+async function main() {
+  /*
+A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+so nftContract here is a factory for instances of our GameItem contract.
+*/
+  const nftContract = await ethers.getContractFactory("GameItem");
+
+  // here we deploy the contract
+  const deployedNFTContract = await nftContract.deploy();
+
+  // wait for the contract to deploy
+  await deployedNFTContract.deployed();
+
+  // print the address of the deployed contract
+  console.log("NFT Contract Address:", deployedNFTContract.address);
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
